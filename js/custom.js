6ff4ab0a203b98fb2e6abbc435933b0e32ce8dbb
Rohithgilla12 <gillarohith1@gmail.com>
2020-03-19 20:45:07 +0530

    State wise graphs

--- js/custom.js
+++ js/custom.js
@@ -11,8 +11,31 @@ function displayNotification() {
 }
 
 var apiData = [];
+var apiStateData = [];
 var mapTotalData = [];
 var dailyCases = [];
+var stateCases = [];
+var hardStateCases = [
+  { "Andhra Pradesh": 2 },
+  { Maharashtra: 49 },
+  { Chhattisgarh: 18 },
+  { Chandigarh: 17 },
+  { Pondicherry: 16 },
+  { "West Bengal": 15 },
+  { Odisha: 14 },
+  { Uttarakhand: 13 },
+  { Karnataka: 34 },
+  { Punjab: 11 },
+  { "Jammu and Kashmir": 10 },
+  { "Tamil Nadu": 9 },
+  { Ladakh: 8 },
+  { "Uttar Pradesh": 19 },
+  { Telangana: 13 },
+  { Rajasthan: 7 },
+  { Kerala: 28 },
+  { Haryana: 17 },
+  { Delhi: 12 }
+];
 
 var now = new Date();
 var millisTill20 =
@@ -22,6 +45,27 @@ var millisTill20 =
 var ctx = document.getElementById("myChart").getContext("2d");
 
 var ctxDaily = document.getElementById("newDailyCases").getContext("2d");
+
+var stateCtx = document.getElementById("stateCases").getContext("2d");
+
+const createTempGraph = () => {
+  var localMapData = [];
+  for (state in hardStateCases) {
+    localMapData.push({
+      x: Object.keys(hardStateCases[state])[0],
+      y: Object.values(hardStateCases[state])[0]
+    });
+  }
+  return localMapData;
+};
+
+const sort_by_key = (array, key) => {
+  return array.sort(function(a, b) {
+    var x = a[key];
+    var y = b[key];
+    return x < y ? -1 : x > y ? 1 : 0;
+  });
+};
 
 const createMapArr = queryParam => {
   var localMapData = [];
@@ -50,13 +94,96 @@ const createNewaDailyArr = () => {
   return localMapData;
 };
 
+const createStateArr = () => {
+  var localData = [];
+  for (dataPoint in apiStateData) {
+    let data = apiStateData[dataPoint];
+    console.log(data);
+    localData.push({
+      x: data.loc,
+      y: data.confirmedCasesIndian + data.confirmedCasesForeign
+    });
+  }
+  return localData;
+};
+
 Chart.defaults.global.defaultFontColor = "white";
+
+stateCases = createTempGraph();
+stateCases = sort_by_key(stateCases, "y");
+var myLineChart = new Chart(stateCtx, {
+  type: "bar",
+  data: {
+    labels: stateCases.map(function(e) {
+      return e.x;
+    }),
+    datasets: [
+      {
+        label: "Total Cases",
+        data: stateCases.map(function(e) {
+          return e.y;
+        }),
+        backgroundColor: "rgba(240, 223, 135, 0.5)",
+        borderColor: "#FFF222",
+        borderWidth: 1
+      }
+    ]
+  },
+  scaleFontColor: "#FFFFFF",
+  options: {
+    // responsive: false,
+    maintainAspectRatio: false,
+    title: {
+      display: true,
+      text: "State vs Total Cases in India"
+    },
+    animation: {
+      duration: 2000,
+      easing: "linear"
+    },
+    scales: {
+      xAxes: [
+        {
+          scaleLabel: {
+            display: true,
+            labelString: "State"
+          },
+          gridLines: {
+            color: "#660066",
+            zeroLineColor: "white",
+            zeroLineWidth: 2
+          },
+          ticks: {
+            autoSkip: true
+          }
+        }
+      ],
+      yAxes: [
+        {
+          gridLines: {
+            color: "#660066",
+            zeroLineColor: "white",
+            zeroLineWidth: 2
+          },
+          scaleLabel: {
+            display: true,
+            labelString: "Total Cases"
+          },
+          ticks: {
+            autoSkip: true,
+            maxTicksLimit: 4
+          }
+        }
+      ]
+    }
+  }
+});
 $.when(
   $.ajax("https://api.rootnet.in/covid19-in/stats/daily").then(response => {
     apiData = response["data"];
     mapTotalData = createMapArr();
     dailyCases = createNewaDailyArr();
-    console.log(dailyCases);
+
     var myLineChart = new Chart(ctx, {
       type: "line",
       data: {
