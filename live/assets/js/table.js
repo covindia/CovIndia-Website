6d8c02706dcdd5e9c6eae0b7bd677c7f1812a487
Rohithgilla12 <gillarohith1@gmail.com>
2020-03-30 12:49:37 +0530

    seekbar

--- live/assets/js/table.js
+++ live/assets/js/table.js
@@ -1,30 +1,39 @@
 var stateData = [];
 $.when(
-  $.ajax("https://v1.api.covindia.com/daily-states-complete").then(
-    response => {
-      console.log(response);
-      stateData = response;
-      const data = Object.keys(stateData).map(key => {
-        return [
-          key,
-          stateData[key].TotalCases,
-          stateData[key].NewCases,
-          stateData[key].TotalDeaths,
-          stateData[key].NewDeaths
-        ];
-      });
-      var table = $("#states").DataTable({
-        data: data,
-        pageLength: 5,
-        dom: "Bfrtip",
-        buttons: ["copyHtml5", "csvHtml5", "pdfHtml5", "excelHtml5"],
-        paging: false,
-        title: "Confirmed Cases and Deaths by state in India"
-      });
-      table
-        .buttons()
-        .container()
-        .appendTo("#states_wrapper .col-md-6:eq(0)");
-    }
-  )
+  $.ajax("https://v1.api.covindia.com/daily-states-complete").then(response => {
+    console.log(response);
+    stateData = response;
+    const data = Object.keys(stateData).map(key => {
+      return [
+        key,
+        stateData[key].TotalCases,
+        stateData[key].NewCases,
+        stateData[key].TotalDeaths,
+        stateData[key].NewDeaths
+      ];
+    });
+    var table = $("#states").DataTable({
+      data: data,
+      pageLength: 5,
+      dom: "Bfrtip",
+      buttons: ["copyHtml5", "csvHtml5", "pdfHtml5", "excelHtml5"],
+      paging: false,
+      title: "Confirmed Cases and Deaths by state in India"
+    });
+    table
+      .buttons()
+      .container()
+      .appendTo("#states_wrapper .col-md-6:eq(0)");
+  })
 );
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.min = new Date("2020.03.02").getDate();
+slider.max = new Date().getDate();
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+};
