46a0d02b850ab3f570593d4b21833b75cd7b03b7
Rohithgilla12 <gillarohith1@gmail.com>
2020-04-01 00:26:44 +0530

    Added table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import WorldStats from "./components/WorldStats";
+import WorldTable from "./components/WorldTable";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
           <WorldStats/>
           {/* <WorldGraphs /> */}
-          {/* <WorldTable /> */}
+          <WorldTable/>
       </header>
     </div>
   );
