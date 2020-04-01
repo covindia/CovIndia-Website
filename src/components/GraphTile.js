8bfed7b539a6d19f5cb3a64e9a4f186551f2b6b1
Rohithgilla12 <gillarohith1@gmail.com>
2020-04-01 18:40:16 +0530

    Tick settings

--- src/components/GraphTile.js
+++ src/components/GraphTile.js
@@ -1,17 +1,25 @@
-import {Area, AreaChart, CartesianGrid, Label, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  Label,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis
+} from "recharts";
 import React from "react";
-import styled from 'styled-components';
-
+import styled from "styled-components";
 
 const ToolTipWrapper = styled.div`
-    color:black;
-    background-image: linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%);
+  color: black;
+  background-image: linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%);
 `;
 
 const GradCard = styled.div`
   border-radius: 25px;
   background: linear-gradient(#20033c 0%, #303 100%);
-  box-shadow: 5px 5px 7px rgba(0, 0, 0, 0.53);  
+  box-shadow: 5px 5px 7px rgba(0, 0, 0, 0.53);
   height: 320px;
   display: flex;
   align-items: center;
@@ -21,50 +29,59 @@ const GradCard = styled.div`
 `;
 
 const CustomTooltip = ({ active, payload, label }) => {
-    console.log(label,payload);
-    if (active) {
-        return (
-            <ToolTipWrapper className={"custom-tooltip"}>
-                <p className="label">{`Date : ${label}`}</p>
-                <p className={"desc"}> {payload[0].value}</p>
-            </ToolTipWrapper>
-        );
-    }
-
-    return null;
-};
-
-const GraphTile = (props)=>{
-    return(
-        <GradCard>
-        <ResponsiveContainer height={300}>
-            <AreaChart
-                data={props.data}
-                >
-                <defs>
-                    <linearGradient id="colorDeath" x1="0" y1="0" x2="0" y2="1">
-                        <stop offset="5%" stopColor="#958686" stopOpacity={0.8}/>
-                        <stop offset="95%" stopColor="#958686" stopOpacity={0.2}/>
-                    </linearGradient>
-                    <linearGradient id="colorInfected" x1="0" y1="0" x2="0" y2="1">
-                        <stop offset="5%" stopColor="#eada0a" stopOpacity={0.8}/>
-                        <stop offset="95%" stopColor="#eada0a" stopOpacity={0.2}/>
-                    </linearGradient>
-                    <linearGradient id="colorCured" x1="0" y1="0" x2="0" y2="1">
-                        <stop offset="5%" stopColor="#00ef00" stopOpacity={0.8}/>
-                        <stop offset="95%" stopColor="#00ef00" stopOpacity={0.2}/>
-                    </linearGradient>
-                </defs>
-                {/*<CartesianGrid strokeDasharray="4 4"/>*/}
-                <XAxis dataKey={"reportDate"} value="Date"  stroke="white" />
-                <Label value="Date" offset={0} position="insideBottom" />
-                <YAxis stroke="white" />
-                <Area type="monotone" dataKey={props.dataKey} fillOpacity={1} stroke={props.stroke} fill={`url(#${props.fill})`}/>
-                <Tooltip  content={CustomTooltip} />
-            </AreaChart>
-        </ResponsiveContainer>
-        </GradCard>
+  console.log(label, payload);
+  if (active) {
+    return (
+      <ToolTipWrapper className={"custom-tooltip"}>
+        <p className="label">{`Date : ${label}`}</p>
+        <p className={"desc"}> {payload[0].value}</p>
+      </ToolTipWrapper>
     );
+  }
+
+  return null;
 };
 
-export  default GraphTile;
+const GraphTile = props => {
+  return (
+    <GradCard>
+      <ResponsiveContainer height={300}>
+        <AreaChart data={props.data}>
+          <defs>
+            <linearGradient id="colorDeath" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#958686" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#958686" stopOpacity={0.2} />
+            </linearGradient>
+            <linearGradient id="colorInfected" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#eada0a" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#eada0a" stopOpacity={0.2} />
+            </linearGradient>
+            <linearGradient id="colorCured" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#00ef00" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#00ef00" stopOpacity={0.2} />
+            </linearGradient>
+          </defs>
+          {/*<CartesianGrid strokeDasharray="4 4"/>*/}
+          <XAxis
+            dataKey={"reportDate"}
+            value="Date"
+            stroke="white"
+            tickFormatter={tick => tick.replace("2020-", "")}
+          />
+          <Label value="Date" offset={0} position="insideBottom" />
+          <YAxis stroke="white" />
+          <Area
+            type="monotone"
+            dataKey={props.dataKey}
+            fillOpacity={1}
+            stroke={props.stroke}
+            fill={`url(#${props.fill})`}
+          />
+          <Tooltip content={CustomTooltip} />
+        </AreaChart>
+      </ResponsiveContainer>
+    </GradCard>
+  );
+};
+
+export default GraphTile;
