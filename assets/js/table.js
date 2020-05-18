4abfbd553d2930d83394b2dfdfdbb08a42864419
Achal Agarwal <1169@Mechyd.ad>
2020-05-18 05:44:18 +0530

    reverting svg import

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,6 +1,6 @@
 var stateData = [];
 $.when(
-  $.ajax("https://v1.api.covindia.com/table-data").then(
+  $.ajax("https://v1.api.covindia.com/daily-states-complete").then(
     (response) => {
       //console.log(response);
       console.log("Dummy")
